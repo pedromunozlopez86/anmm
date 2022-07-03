4302b8e7615add28c4d2034aa388f7d1d0602587
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-07-03 18:32:21 -0400

    refactor: nav bar sin primevue

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,61 +1,45 @@
 <script setup>
-import Menubar from 'primevue/menubar'
-import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-const items = ref([
-    {
-        label: 'Inicio',
-    },
-    {
-        label: 'Programa',
-    },
-    {
-        label: 'Informaciones',
-    },
-    {
-        label: 'Comisiones',
-        items: [
-            {
-                label: 'Gobierno Judicial'
-            },
-            {
-                label: 'Infancia y Familia'
-            },
-            {
-                label: 'Jubilación Digna y Fraterna'
-            }
-        ]
-    },
-    {
-        label: 'Documentos',
-        items: [
-            {
-                label: ''
-            }
-        ]
-    },
-    {
-        label: 'Contacto',
-    },
+const router = useRouter()
 
-])
 </script>
 <template>
-    <Menubar :model="items" />
+    <ul class="flex gap-5 pr-8 mt-0 list-none text-lg text-marino justify-content-end align-items-center navbar">
+        <li>
+            <RouterLink :to="{ name: 'Inicio' }">Inicio</RouterLink>
+        </li>
+        <li>
+            <RouterLink :to="{ name: 'Programa' }">Programa</RouterLink>
+        </li>
+        <li>
+            <RouterLink :to="{ name: 'Informaciones' }">Informaciones</RouterLink>
+        </li>
+        <li>
+            <RouterLink :to="{ name: 'Comisiones' }">Comisiones</RouterLink>
+        </li>
+        <li>
+            Documentos
+        </li>
+        <li>
+            <RouterLink :to="{ name: 'Contacto' }">Contacto</RouterLink>
+        </li>
+    </ul>
 </template>
 <style scoped lang="scss">
-.p-menubar {
-    display: flex;
-    justify-content: end;
+.navbar {
+    height: 4.5rem;
     background: linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0.25) 100%);
     opacity: 0.7;
-    border: none;
-    padding: 1rem 0;
 }
 
-::v-deep(.p-menubar-root-list) {
-    padding-right: 3rem !important;
-    display: flex;
-    font-size: large !important;
+a {
+    color: var(--azul-marino);
+}
+
+.router-link-active {
+    border-bottom: 3px solid var(--azul-marino);
+    font-weight: 900;
+    transition: all .1s;
 }
 </style>
