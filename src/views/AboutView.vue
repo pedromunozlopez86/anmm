e81aee0158b7ae2877dc63cbc6875e789113be08
Pedro Muñoz <pmunoz@pegasus.cl>
2022-07-03 15:40:18 -0400

    que wea

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Button from "primevue/button";
+console.log("sdasds");
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h5>Basic</h5>
+    <Button label="Submit" />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
