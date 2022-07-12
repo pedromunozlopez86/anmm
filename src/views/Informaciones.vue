8fee91f4122607001fe256c7551895c220ca3837
Pedro Muñoz <pmunoz@pegasus.cl>
2022-07-12 18:36:56 -0400

    esta no

--- src/views/Informaciones.vue
+++ src/views/Informaciones.vue
@@ -1,12 +1,23 @@
 <script setup>
-
+import TitleContentVue from "../components/TitleContent.vue";
+import TitleContent from "../components/TitleContent.vue";
 </script>
 <template>
-    <div>
-        INFORMACIONES
-    </div>
+  <div>
+    <section id="hero-section"></section>
+    <TitleContent title="Informaciones" title-icon="pi-info">
+      <div>hola</div>
+    </TitleContent>
+  </div>
 </template>
 
-
 <style lang="scss" scoped>
+#hero-section {
+  background-image: url("@/assets/img/hero-informaciones.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  margin-top: -5rem;
+  height: 26.5625rem;
+}
 </style>
