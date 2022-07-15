d488f33eb1041ee578f5d920e65d14ef6e828a0d
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-07-14 20:35:06 -0400

    feat: formulario inscripcion parte 2

--- src/views/FormularioInscripcion.vue
+++ src/views/FormularioInscripcion.vue
@@ -1,29 +1,65 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import ToggleButton from 'primevue/togglebutton'
 import InputText from 'primevue/inputtext'
 import Dropdown from 'primevue/dropdown'
-import SelectButton from 'primevue/selectbutton'
+import Button from 'primevue/button'
 import Fieldset from 'primevue/fieldset'
 import TitleContent from '../components/TitleContent.vue'
 import regionalOptions from '@/assets/data/regional.json'
 
-const inscripcion = reactive({
+const form = reactive({
   nombreCompleto: '',
   rut: '',
   telefono: '',
   correo: '',
   regional: '',
   vieneAcompanante: false,
-  nombreCompletoAcompanante: '',
-  rutAcompanante: '',
-  telefonoAcompanante: '',
-  correoAcompanante: '',
-  nombreEmergencia: '',
-  telefonoEmergencia: '',
+  acompanante: {
+    nombre: '',
+    rut: '',
+    telefono: '',
+    correo: '',
+  },
+  contactoEmergencia: {
+    nombre: '',
+    telefono: '',
+  },
   restriccionAlimentaria: false,
   comentarioRestriccionAlimentaria: '',
+  trasladoPuertoVaras: '',
+  requiereTransporteHotel: false,
+  requiereTrasladoCeremonia: false,
+  incluyePaseo: false,
+  paseoIncluyeAcompanante: false,
+  incluyeFiesta: false,
+  fiestaIncluyeAcompanante: false,
+})
+
+const inscripcion = computed(() => {
+  if (form.incluyePaseo) {
+    return {
+      total: '250.000',
+      detalle: 'Inscripción General Asociado + Paseo',
+    }
+  }
+  if (form.vieneAcompanante) {
+    return {
+      total: '300.000',
+      detalle: 'Inscripción General Asociado + Acompañante',
+    }
+  }
+  if (form.vieneAcompanante && form.incluyePaseo) {
+    return {
+      total: '460.000',
+      detalle: 'Inscripción General Asociado + Acompañante + Paseo',
+    }
+  }
+  return {
+    total: '170.000',
+    detalle: 'Inscripción General Asociado',
+  }
 })
 
 const router = useRouter()
@@ -38,6 +74,31 @@ const acompananteOptions = ref([
     value: true,
   },
 ])
+
+const trasladoPVOptions = ref([
+  { nombre: 'Aéreo', value: 'aereo' },
+  { nombre: 'Terrestre', value: 'terrestre' },
+])
+
+function sendPagoML() {
+  if (form.incluyePaseo) {
+    window.location.href = 'https://mpago.la/21LS4hs' // 250
+    return
+  }
+  if (form.vieneAcompanante) {
+    window.location.href = 'https://mpago.la/2Yf6TvB' // 300
+    return
+  }
+  if (form.vieneAcompanante && form.incluyePaseo) {
+    window.location.href = 'https://mpago.la/2zvo2ex' // 460
+    return
+  }
+  window.location.href = 'https://mpago.la/33ujzpJ' // 170
+}
+
+function sendPagoPlanilla() {
+  // TODO
+}
 </script>
 <template>
   <section id="hero-section"></section>
@@ -54,7 +115,7 @@ const acompananteOptions = ref([
               id="nombre"
               placeholder="Nombre"
               type="text"
-              v-model="inscripcion.nombreCompleto"
+              v-model="form.nombreCompleto"
             />
           </div>
         </div>
@@ -64,7 +125,7 @@ const acompananteOptions = ref([
               id="rut"
               placeholder="RUT"
               type="text"
-              v-model="inscripcion.rut"
+              v-model="form.rut"
             />
           </div>
         </div>
@@ -74,7 +135,7 @@ const acompananteOptions = ref([
               id="telefono"
               placeholder="Teléfono"
               type="tel"
-              v-model="inscripcion.telefono"
+              v-model="form.telefono"
             />
           </div>
         </div>
@@ -84,14 +145,14 @@ const acompananteOptions = ref([
               id="correo"
               placeholder="Correo Electrónico"
               type="email"
-              v-model="inscripcion.correo"
+              v-model="form.correo"
             />
           </div>
         </div>
         <div class="col-12 md:col-6">
           <div class="p-inputgroup">
             <Dropdown
-              v-model="inscripcion.regional"
+              v-model="form.regional"
               :options="regionalOptions"
               optionLabel="nombre"
               optionValue="nombre"
@@ -103,11 +164,13 @@ const acompananteOptions = ref([
         </div>
         <div class="col-12 md:col-6">
           <div class="p-inputgroup flex justify-content-center">
-            <SelectButton
-              v-model="inscripcion.vieneAcompanante"
-              :options="acompananteOptions"
-              optionLabel="label"
-              optionValue="value"
+            <ToggleButton
+              id="tiene-acompanante"
+              v-model="form.vieneAcompanante"
+              onLabel="Con Acopañante"
+              offLabel="Sin Acompañante"
+              onIcon="pi pi-check"
+              offIcon="pi pi-times"
             />
           </div>
         </div>
@@ -115,7 +178,7 @@ const acompananteOptions = ref([
     </Fieldset>
     <Fieldset
       legend="Datos Acompañante"
-      v-if="inscripcion.vieneAcompanante"
+      v-if="form.vieneAcompanante"
       class="mb-3"
     >
       <div class="grid">
@@ -125,7 +188,7 @@ const acompananteOptions = ref([
               id="nombre-acompanante"
               placeholder="Nombre Acompañante"
               type="text"
-              v-model="inscripcion.nombreCompletoAcompanante"
+              v-model="form.acompanante.nombre"
             />
           </div>
         </div>
@@ -135,7 +198,7 @@ const acompananteOptions = ref([
               id="rut-acompanante"
               placeholder="RUT Acompañante"
               type="text"
-              v-model="inscripcion.rutAcompanante"
+              v-model="form.acompanante.rut"
             />
           </div>
         </div>
@@ -145,7 +208,7 @@ const acompananteOptions = ref([
               id="telefono-acompanante"
               placeholder="Teléfono Acompañante"
               type="tel"
-              v-model="inscripcion.telefonoAcompanante"
+              v-model="form.acompanante.telefono"
             />
           </div>
         </div>
@@ -155,7 +218,7 @@ const acompananteOptions = ref([
               id="correo-acompanante"
               placeholder="Correo Acompañante"
               type="email"
-              v-model="inscripcion.correoAcompanante"
+              v-model="form.acompanante.correo"
             />
           </div>
         </div>
@@ -169,7 +232,7 @@ const acompananteOptions = ref([
               id="nombre-emergencia"
               placeholder="Nombre Contacto"
               type="text"
-              v-model="inscripcion.nombreEmergencia"
+              v-model="form.contactoEmergencia.nombre"
             />
           </div>
         </div>
@@ -179,7 +242,7 @@ const acompananteOptions = ref([
               id="telefono-emergencia"
               placeholder="Teléfono Contacto"
               type="tel"
-              v-model="inscripcion.telefonoEmergencia"
+              v-model="form.contactoEmergencia.nombre"
             />
           </div>
         </div>
@@ -187,7 +250,7 @@ const acompananteOptions = ref([
           <div class="p-inputgroup flex justify-content-center">
             <ToggleButton
               id="tiene-restriccion-alimentaria"
-              v-model="inscripcion.restriccionAlimentaria"
+              v-model="form.restriccionAlimentaria"
               onLabel="Tengo Restricción Alimentaria"
               offLabel="No Tengo Restricción Alimentaria"
               onIcon="pi pi-check"
@@ -195,15 +258,86 @@ const acompananteOptions = ref([
             />
           </div>
         </div>
-        <div class="col-12 md:col-6" v-if="inscripcion.restriccionAlimentaria">
+        <div class="col-12 md:col-6" v-if="form.restriccionAlimentaria">
           <div class="p-inputgroup">
             <InputText
               id="comentario-restriccion-alimentaria"
               placeholder="Especifíque su Restriccion"
               type="tel"
-              v-model="inscripcion.comentarioRestriccionAlimentaria"
+              v-model="form.comentarioRestriccionAlimentaria"
             />
           </div>
+        </div>
+      </div>
+    </Fieldset>
+    <Fieldset legend="Traslado" class="mb-3">
+      <div class="grid">
+        <div class="col-12 md:col-6">
+          <div class="p-inputgroup">
+            <Dropdown
+              id="traslado-puerto-varas"
+              v-model="form.trasladoPuertoVaras"
+              :options="trasladoPVOptions"
+              optionLabel="nombre"
+              optionValue="value"
+              placeholder="Traslado hacia Puerto Varas"
+            />
+          </div>
+        </div>
+        <div
+          class="col-12 md:col-6"
+          v-if="form.trasladoPuertoVaras === 'aereo'"
+        >
+          <div class="p-inputgroup flex justify-content-center">
+            <ToggleButton
+              id="requiere-transporte-hotel"
+              v-model="form.requiereTransporteHotel"
+              onLabel="Con Transporte Aeropuerto-Hotel-Aeropuerto"
+              offLabel="Sin Transporte Aeropuerto-Hotel-Aeropuerto"
+              onIcon="pi pi-check"
+              offIcon="pi pi-times"
+            />
+          </div>
+        </div>
+        <div class="col-12 md:col-6">
+          <div class="p-inputgroup flex justify-content-center">
+            <ToggleButton
+              id="requiere-transporte-hotel"
+              v-model="form.requiereTrasladoCeremonia"
+              onLabel="Con Traslado a Ceremonia Inauguración"
+              offLabel="Sin Traslado a Ceremonia Inauguración"
+              onIcon="pi pi-check"
+              offIcon="pi pi-times"
+            />
+          </div>
+        </div>
+      </div>
+    </Fieldset>
+    <Fieldset legend="Pago">
+      <div class="grid">
+        <div
+          class="p-2 col-12 flex flex-column justify-content-center align-items-center"
+        >
+          <h2 class="my-0">$ {{ inscripcion.total }}.-</h2>
+          <p class="mt-1 mb-3 text-sm">{{ inscripcion.detalle }}</p>
+        </div>
+        <div
+          class="col-12 md:col-6 flex justify-content-center align-items-center"
+        >
+          <Button
+            label="Pagar con Mercado Pago"
+            class="px-3"
+            @click="sendPagoML"
+          />
+        </div>
+        <div
+          class="col-12 md:col-6 flex justify-content-center align-items-center"
+        >
+          <Button
+            label="Descuento por Planilla"
+            class="px-3"
+            @click="sendPagoPlanilla"
+          />
         </div>
       </div>
     </Fieldset>
