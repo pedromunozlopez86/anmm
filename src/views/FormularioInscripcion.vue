66bf11038e74b1d70db52557fff8077854e04b65
Pedro Muñoz <pmunoz@pegasus.cl>
2022-07-17 19:04:09 -0400

    add campos formu

--- src/views/FormularioInscripcion.vue
+++ src/views/FormularioInscripcion.vue
@@ -1,111 +1,121 @@
 <script setup>
-import { computed, reactive, ref } from 'vue'
-import emailjs from 'emailjs-com'
-import ToggleButton from 'primevue/togglebutton'
-import InputText from 'primevue/inputtext'
-import Dropdown from 'primevue/dropdown'
-import Button from 'primevue/button'
-import Fieldset from 'primevue/fieldset'
-import TitleContent from '../components/TitleContent.vue'
-import regionalOptions from '@/assets/data/regional.json'
-import { useEmailJs } from '../composables'
+import { computed, reactive, ref } from "vue";
+import emailjs from "emailjs-com";
+import ToggleButton from "primevue/togglebutton";
+import InputText from "primevue/inputtext";
+import Dropdown from "primevue/dropdown";
+import Button from "primevue/button";
+import Fieldset from "primevue/fieldset";
+import TitleContent from "../components/TitleContent.vue";
+import regionalOptions from "@/assets/data/regional.json";
+import { useEmailJs } from "../composables";
 
-const { template, sendEmail } = useEmailJs()
+const { template, sendEmail } = useEmailJs();
 
 const form = reactive({
-  nombreCompleto: '',
-  rut: '',
-  telefono: '',
-  correo: '',
-  regional: '',
+  nombreCompleto: "",
+  rut: "",
+  telefono: "",
+  correo: "",
+  regional: "",
   vieneAcompanante: false,
   acompanante: {
-    nombre: '',
-    rut: '',
-    telefono: '',
-    correo: '',
+    nombre: "",
+    rut: "",
+    telefono: "",
+    correo: "",
   },
   contactoEmergencia: {
-    nombre: '',
-    telefono: '',
+    nombre: "",
+    telefono: "",
   },
   restriccionAlimentaria: false,
-  comentarioRestriccionAlimentaria: '',
-  trasladoPuertoVaras: '',
+  comentarioRestriccionAlimentaria: "",
+  trasladoPuertoVaras: "",
   requiereTransporteHotel: false,
   requiereTrasladoCeremonia: false,
   incluyePaseo: false,
   incluyeFiesta: false,
-  formaPago: '',
-})
+  formaPago: "",
+  cuotas: "",
+  valorFinal: "",
+});
 
-const isLoading = ref(false)
+const isLoading = ref(false);
 
 const inscripcion = computed(() => {
   if (form.incluyePaseo && !form.vieneAcompanante) {
     return {
-      total: '250.000',
-      detalle: 'Inscripción General Asociado + Paseo',
-    }
+      total: "250.000",
+      detalle: "Inscripción General Asociado + Paseo",
+    };
   }
   if (form.vieneAcompanante && !form.incluyePaseo) {
     return {
-      total: '300.000',
-      detalle: 'Inscripción General Asociado + Acompañante',
-    }
+      total: "300.000",
+      detalle: "Inscripción General Asociado + Acompañante",
+    };
   }
   if (form.vieneAcompanante && form.incluyePaseo) {
     return {
-      total: '460.000',
-      detalle: 'Inscripción General Asociado + Acompañante + Paseo',
-    }
+      total: "460.000",
+      detalle: "Inscripción General Asociado + Acompañante + Paseo",
+    };
   }
   return {
-    total: '170.000',
-    detalle: 'Inscripción General Asociado',
-  }
-})
+    total: "170.000",
+    detalle: "Inscripción General Asociado",
+  };
+});
 
 const trasladoPVOptions = ref([
-  { nombre: 'Aéreo', value: 'aereo' },
-  { nombre: 'Terrestre', value: 'terrestre' },
-])
+  { nombre: "Aéreo", value: "aereo" },
+  { nombre: "Terrestre", value: "terrestre" },
+]);
+
+const cuotasOptions = ref([
+  { nombre: "1", value: 1 },
+  { nombre: "2", value: 2 },
+  { nombre: "3", value: 3 },
+]);
 
 function redirectToML() {
   if (form.incluyePaseo && !form.vieneAcompanante) {
-    window.location.href = 'https://mpago.la/21LS4hs' // 250
-    return
+    window.location.href = "https://mpago.la/21LS4hs"; // 250
+    return;
   }
   if (form.vieneAcompanante && !form.incluyePaseo) {
-    window.location.href = 'https://mpago.la/2Yf6TvB' // 300
-    return
+    window.location.href = "https://mpago.la/2Yf6TvB"; // 300
+    return;
   }
   if (form.vieneAcompanante && form.incluyePaseo) {
-    window.location.href = 'https://mpago.la/2zvo2ex' // 460
-    return
+    window.location.href = "https://mpago.la/2zvo2ex"; // 460
+    return;
   }
-  window.location.href = 'https://mpago.la/1a8EEiF' // 170
+  window.location.href = "https://mpago.la/1a8EEiF"; // 170
 }
 
 async function pagarPorML() {
-  isLoading.value = true
-  form.formaPago = 'Mercado Libre'
+  isLoading.value = true;
+  form.formaPago = "Mercado Libre";
+  form.valorFinal = inscripcion.value;
   await sendEmail(template.INSCRIPCION, form).then((res) => {
     if (res.status === 200) {
-      redirectToML()
+      redirectToML();
     }
-  })
+  });
 }
 
 async function pagarPorPlanilla() {
-  isLoading.value = true
-  form.formaPago = 'Descuento por Planilla'
+  isLoading.value = true;
+  form.formaPago = "Descuento por Planilla";
+  form.valorFinal = inscripcion.value;
   await sendEmail(template.INSCRIPCION, form).then((res) => {
     if (res.status === 200) {
-      isLoading.value = false
-      console.log('exito')
+      isLoading.value = false;
+      console.log("exito");
     }
-  })
+  });
 }
 </script>
 <template>
@@ -370,6 +380,14 @@ async function pagarPorPlanilla() {
         <div
           class="col-12 md:col-6 flex justify-content-center align-items-center"
         >
+          <Dropdown
+            id="cuotas"
+            v-model="form.cuotas"
+            :options="cuotasOptions"
+            optionLabel="nombre"
+            optionValue="value"
+            placeholder="N° de Cuotas"
+          />
           <Button
             label="Descuento por Planilla"
             class="px-3"
@@ -384,7 +402,7 @@ async function pagarPorPlanilla() {
 
 <style lang="scss" scoped>
 #hero-section {
-  background-image: url('@/assets/img/hero-contacto.png');
+  background-image: url("@/assets/img/hero-contacto.png");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center center;
